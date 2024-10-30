7b3d3f5dea6a1d178222194bdfabc75f64b7a13f
christiancajiao <christiancamilocajiaochacon@gmail.com>
2024-10-30 17:16:58 -0500

    new card and grid system

--- portafolio-vite/src/App.tsx
+++ portafolio-vite/src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <section className="first_section">
-        <div className="container">
+        <div className="container_top">
           <div className="first_name">
             <h1 className="child">CHRISTIAN</h1>
             <h1 className="child">CAMILO</h1>
@@ -20,9 +16,30 @@ function App() {
           </div>
         </div>
       </section>
-      <section>
-        <div style={{height: "200vh"}}>
-          <h2>FEATURES</h2>
+      <section className="container section">
+        <div className="card">
+          <div className="card-inner">
+            <div className="card-front">
+              <div className="card_img"></div>
+              <div className="card_fotter"></div>
+              <div className="card_content">
+                <p>Name: Christian Camilo Cajiao Chacon</p>
+                <p>Location: Cali, Colombia</p>
+                <p>Idioms: English -advance-  Spanish -native-</p>
+              </div>
+            </div>
+            <div className="card-back">
+              <h1>card back</h1>
+              <p>
+                  Hi, I'm Christian Cajiao, a self-taught web developer
+                  specializing in front-end development with React. I’m
+                  currently expanding my skills to become a full-stack
+                  developer, diving into Node.js and Next.js. With a keen eye
+                  for design and a passion for detail, I strive to create
+                  seamless, user-friendly digital experiences that stand out.
+                </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
